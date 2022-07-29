bb6d2a164c4a67064d172e631f666f1da6803467
tconns <thanhcongns94@gmail.com>
2022-07-29 11:45:10 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { ProgressPlugin } = require('webpack');
+
+module.exports = function(env, argv) {
+    return {
+        mode: argv.mode,
+        entry: path.resolve(__dirname, 'src/index.tsx'),
+        output: {
+            filename: 'index.js',
+            library: '$',
+            libraryTarget: 'umd',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|ts)x?$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'ts-loader']
+                }
+            ]
+        },
+        externals: {
+            'react': 'react',
+            'react-dom': 'react-dom'
+        },
+        resolve: {
+            alias: {
+                '@root': path.resolve(__dirname, 'src')
+            },
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        plugins: [
+            new ProgressPlugin(),
+            new ESLintPlugin({
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }),
+            new CleanWebpackPlugin()
+        ]
+    };
+}
